00b9ea6c8997488d0fff5ac5768b5a0696c7032c
ayatKhurram <ayatkhurram55@gmail.com>
2025-01-23 20:07:10 +0500

    Add files via upload

---
+++ Fibonacci.py
@@ -0,0 +1,14 @@
+def fibonacci(n):
+   
+    if n<=0:
+        return 0
+    elif n==1:
+        return 1
+    else:
+        return fibonacci(n-1)+fibonacci(n-2)
+
+num_terms=int(input("How much numbers in series you want? "))
+
+print("Fibonacci Series:")
+for i in range(num_terms):
+    print(fibonacci(i),end=" ")
